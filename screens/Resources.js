c1796264a670f9ae69a45c6cd6d87d915a991f69
Upsham Naik <upshamgnaik@outlook.com>
2022-02-22 21:31:25 -0500

    test

--- screens/Resources.js
+++ screens/Resources.js
@@ -1,12 +1,15 @@
 import * as React from 'react';
-import { View, ScrollView, Text} from 'react-native';
+import { View, ScrollView, Text, Button} from 'react-native';
 import { LinearGradient } from "expo-linear-gradient";
 import { useState, useEffect } from "react";
 import { Box, FlatList, Center, NativeBaseProvider } from "native-base";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Resources({navigation}){
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [show, setShow] = useState(true);
+    const toggleText = () => setShow(show => !show)
 
     const fetchData = async () => {
       const resp = await fetch("https://apis.yinftw.com/nb/resources/food");
@@ -17,7 +20,7 @@ export default function Resources({navigation}){
 
     const renderItem = ({ item }) => {
       return (
-        <Box px={5} py={2} rounded="md" bg="primary.800" my={2}>
+        <Box px={5} py={2} rounded="md" bg="primary.300" my={2}>
           {item.Name}
         </Box>
       )
@@ -28,20 +31,26 @@ export default function Resources({navigation}){
     }, []);
 
     return(
-      <ScrollView>
-        <NativeBaseProvider>
-          <Center flex={1}>
-          {loading && <Box>Loading..</Box>}
-          <Box>Using Fetch API</Box>
-          {data && (
-            <FlatList
-              data={data}
-              renderItem={renderItem}
-              keyExtractor={(item) => item.Address}
-            />
-          )}
-          </Center>
-        </NativeBaseProvider>
-      </ScrollView>
+      <SafeAreaView>
+        <Button onPress={() => setShow(show => !show)} title="Show" />
+        {show && ( <SafeAreaView>
+          <ScrollView>
+            <NativeBaseProvider>
+              <Center flex={1}>
+              {loading && <Box>Loading..</Box>}
+              <Box>Using Fetch API</Box>
+              {data && (
+                <FlatList
+                  data={data}
+                  renderItem={renderItem}
+                  keyExtractor={(item) => item.Address}
+                />
+              )}
+              </Center>
+            </NativeBaseProvider>
+          </ScrollView>
+          </SafeAreaView>
+        )}
+      </SafeAreaView>
     )
 }
